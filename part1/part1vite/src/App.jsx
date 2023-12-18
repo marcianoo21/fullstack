c093dcfbf6aeca7fe75970a4b9b6604c40a27409
marcianoo21 <mgrzelak218@gmail.com>
2023-12-19 00:40:53 +0100

    first commit

---
+++ part1/part1vite/src/App.jsx
@@ -0,0 +1,61 @@
+// const App = () => {
+  
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+
+//   return (
+//     <div>
+//       <h1>Hello 'Hello world'</h1>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   return React.createElement(
+//     'div',
+//     null,
+//     React.createElement(
+//       'p', null, 'Hello world, it is ', now.toString()
+//     ),
+//     React.createElement(
+//       'p', null, a, ' plus ', b, ' is ', a + b
+//     )
+//   )
+// }
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>  
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+export default App
